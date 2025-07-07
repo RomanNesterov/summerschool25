78329e18c666a285844f49b6e683ecfde5539032
Roman Nesterov <ranesterovhse@gmail.com>
2025-07-07 11:06:01 +0300

    Day 6 - Lecture slides & Generator.hpp

---
+++ July 7/Generator.hpp
@@ -0,0 +1,15 @@
+//
+//  Generator.hpp
+//  summerschool
+//  Пример интерфейса генератора тестовых данных
+//  Created by Roman N. on 07.07.2025.
+//
+
+#pragma once
+
+class Generator {
+public:
+    void genAscending(int size); // 0, 1, 2, 3, ..., size
+    
+    // другие функции генерации...
+};
